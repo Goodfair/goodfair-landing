a1269ca5c3aeb136f773423cc996ab248b70de1f
KYLE FOLEY <kylef@collabcreators.com>
2018-01-27 19:21:31 -0800

    fix up nav bar and mobile overlay more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,31 @@
 //= require jquery.magnific-popup
 //= require_tree .
 
-$(document).ready(function(){
-  $('#toggle').click(function() {
-     $(this).toggleClass('active');
-     $('#overlay').toggleClass('open');
-    });
+$(document).ready(function() {
+  $('.toggle').click(function() {
+    $('.toggle').toggleClass('active');
+    $('#overlay').toggleClass('open');
+  });
+});
+
+$(window).scroll(function() {
+  // 100 = The point you would like to fade the nav in.
+
+  if ($(window).scrollTop() > 100) {
+
+    $('.navbar').addClass('show');
+
+  } else {
+
+    $('.navbar').removeClass('show');
+
+  };
+});
+
+$('.scroll').on('click', function(e) {
+  e.preventDefault()
+
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 1500);
 });
