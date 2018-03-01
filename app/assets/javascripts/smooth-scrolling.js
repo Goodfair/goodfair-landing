61902655812ef026a05f256369293f0647ed6591
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-02-28 22:06:45 -0800

    auto close menu after cllcking on menu item

--- app/assets/javascripts/smooth-scrolling.js
+++ app/assets/javascripts/smooth-scrolling.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
 
+    $('.toggle').toggleClass('active');
+    $('#overlay').toggleClass('open');
+
     var target = this.hash;
     var $target = $(target);
 
