dde2fca9047f783cee7da8db1f53171dbd410435
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-02-28 22:23:15 -0800

    fix bug auto close overlay menu only for tablet and mobile

--- app/assets/javascripts/smooth-scrolling.js
+++ app/assets/javascripts/smooth-scrolling.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
 
-    $('.toggle').toggleClass('active');
-    $('#overlay').toggleClass('open');
+    if ($(window).width() < '992') {
+      $('.toggle').toggleClass('active');
+      $('#overlay').toggleClass('open');
+    }
 
     var target = this.hash;
     var $target = $(target);
