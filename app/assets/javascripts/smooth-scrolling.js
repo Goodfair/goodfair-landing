8c9126157eacb4c3912ee530e257e012caa70ee1
KYLE FOLEY <kylef@collabcreators.com>
2018-05-30 13:50:35 -0700

    add some more initial protocol tab work, remove smooth scroll since it is hijacking tab links for some reason

--- app/assets/javascripts/smooth-scrolling.js
+++ app/assets/javascripts/smooth-scrolling.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-  $('a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-
-    if ($(window).width() < '992') {
-      $('.toggle').toggleClass('active');
-      $('#overlay').toggleClass('open');
-    }
-
-    var target = this.hash;
-    var $target = $(target);
-
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-    }, 1000, 'swing');
-  });
-});
+// $(document).ready(function(){
+//   $('a[href^="#"]').on('click', function (e) {
+//     e.preventDefault();
+//
+//     if ($(window).width() < '992') {
+//       $('.toggle').toggleClass('active');
+//       $('#overlay').toggleClass('open');
+//     }
+//
+//     var target = this.hash;
+//     var $target = $(target);
+//
+//     $('html, body').animate({
+//       'scrollTop': $target.offset().top
+//     }, 1000, 'swing');
+//   });
+// });
